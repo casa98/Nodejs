5bfe3d8fed480ced7df79c5ed3d50205f47ffbb7
casa98 <cagudeloa@uninorte.edu.co>
2020-09-29 23:36:27 -0500

    Basic routing

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // Function executed everytime server receives a request
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+    }
     
     // Send HTML file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err);
             res.end()
